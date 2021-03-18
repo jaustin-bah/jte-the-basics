f68bf10bf6c10746c1b98c2a5b10d431021523b5
jaustin-bah <80417456+jaustin-bah@users.noreply.github.com>
2021-03-18 13:06:41 -0500

    Update splunk_step_watcher.groovy

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,7 +3,9 @@ void before(){
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
-@AfterStep
-void after(){
-    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
-}
+//@AfterStep
+//void after(){
+//    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
+//}
+
+@AfterStep({ hookContext.step.equals("static_code_analysis") })
